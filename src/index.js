14ef2c072f564568d119a8ef0502ee8c0de9c315
webline-cloud <webline2024@gmail.com>
2024-02-01 08:05:08 +0530

    gsg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-<iframe src="https://links.collect.chat/65bb01b172474ab9834ea3c7"  width="100%" height="600" frameBorder="0"></iframe><script async type="text/javascript" src="https://collectcdn.com/embed.js"></script>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
