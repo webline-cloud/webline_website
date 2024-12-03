174bd3301c7285290c1a1f8cde7b519a5cbee4d1
webline-cloud <153897215+webline-cloud@users.noreply.github.com>
2024-12-03 23:33:41 +0530

    Update TechNews.js

--- src/components/TechNews.js
+++ src/components/TechNews.js
@@ -8,7 +8,9 @@ const TechNews = () => {
     <div>
         <Header/>
         <div className='h-[200vh] example'>
-            <iframe className='w-full h-full example' src="https://write.navneet-kumar.in/" frameborder="0"></iframe>
+            //<iframe className='w-full h-full example' src="https://write.navneet-kumar.in/" frameborder="0"></iframe>
+    <iframe className='w-full h-full example' src="https://write.navneet-kumar.in/" frameBorder="0" sandbox="allow-scripts allow-forms allow-same-origin"></iframe>
+
         </div>
         <Footer />
     </div>
